9ca09db5ccb836ff7699f816071627e0dc807cf2
xuzj <844743779@qq.com>
2018-02-06 14:41:29 +0800

    adqqd

--- sort_algorithm/InsertionSort.py
+++ sort_algorithm/InsertionSort.py
@@ -7,16 +7,17 @@
 重复步骤3，直到找到已排序的元素小于或者等于新元素的位置
 将新元素插入到该位置后
 重复步骤2~5'''
+
 def insert_sort(arry):
     n = len(arry)
     for i in range(1,n):
         if arry[i] < arry[i-1]:
             temp = arry[i]
-            index = i   #带插入的下标
-            for j in range(i-1,-1,-1): #从i-1循环到0
+            index = i   #待插入的下标
+            for j in range(i-1,-1,-1): #从i-1循环到0(包括0)
                 if arry[j] > temp:
                     arry[j+1] = arry[j]
-                    index = j
+                    index = j     #记录待插入下标
                 else:
                     break
 
@@ -25,3 +26,15 @@ def insert_sort(arry):
     return arry
 
 print(insert_sort([4,1,2]))
+
+# def insert(arry):
+#     n = len(arry)
+#     for i in range(1,n):
+#         if arry[i] < arry[i-1]:
+#             temp = arry[i]
+#             index = i
+#             for j in range(i-1,-1,-1):
+#                 if arry[j] > temp:
+#                     arry[j+1] = arry[j]
+#                     index = j
+#
